dee62f1392cb6e89777f7e3098099b2181be3ba6
Yerko Andrei <yerkoandrei@gmail.com>
2023-05-11 23:02:42 -0400

    base del osciloscopio

--- Señal local/Assets/Codigo/Visuales/ControladorOsciloscopio.cs
+++ Señal local/Assets/Codigo/Visuales/ControladorOsciloscopio.cs
@@ -1,18 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ControladorOsciloscopio : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Variables")]
+    [SerializeField] private float velocidadHorizontal;
+    [SerializeField] private float velocidadVertical;
+    [SerializeField] private float máximaSeparación;
+
+    [Header("Referencias visuales")]
+    [SerializeField] private Transform visor;
+    [SerializeField] private Transform luz;
+    [SerializeField] private TrailRenderer rastro;
+
+    [Header("Referencias posiciones")]
+    [SerializeField] private Transform comienzo;
+    [SerializeField] private Transform final;
+
+    private bool subiendo;
+
+    private Vector3 alto;
+    private Vector3 bajo;
+    private Vector3 objetivo;
+
     void Start()
     {
-        
+        visor.position = comienzo.position;
+
+        alto = new Vector3(luz.localPosition.x, luz.localPosition.y + máximaSeparación, luz.localPosition.z);
+        bajo = new Vector3(luz.localPosition.x, luz.localPosition.y - máximaSeparación, luz.localPosition.z);
+
+        subiendo = true;
+        objetivo = alto;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoverVisorHorizontal();
+        MoverVertical();
+    }
+
+    private void MoverVertical()
+    {
+        luz.localPosition = Vector3.MoveTowards(luz.localPosition, objetivo, Time.deltaTime * velocidadVertical);
+
+        if (Vector3.Distance(luz.localPosition, objetivo) <= 0)
+        {
+            if (subiendo)
+                objetivo = bajo;
+            else
+                objetivo = alto;
+
+            subiendo = !subiendo;
+        }
+    }
+
+    private void MoverVisorHorizontal()
+    {
+        visor.position = Vector3.MoveTowards(visor.position, final.position, Time.deltaTime * velocidadHorizontal);
+
+        if (Vector3.Distance(visor.position, final.position) <= 0.01)
+            rastro.emitting = false;
+        else
+            rastro.emitting = true;
+
+        if (Vector3.Distance(visor.position, final.position) <= 0.0001f)
+        {
+            subiendo = true;
+            visor.position = comienzo.position;
+        }
     }
 }
