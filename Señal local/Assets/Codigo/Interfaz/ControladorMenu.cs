f72baa581b507e968fafb2c1737b750eee8341fc
Yerko Andrei <yerkoandrei@gmail.com>
2023-05-15 22:59:48 -0400

    cambio una sola escena

--- Señal local/Assets/Codigo/Interfaz/ControladorMenu.cs
+++ Señal local/Assets/Codigo/Interfaz/ControladorMenu.cs
@@ -1,19 +1,29 @@
 ﻿using System;
-using TMPro;
 using UnityEngine;
-using static Constantes;
+using TMPro;
 
 public class ControladorMenu : MonoBehaviour
 {
+    [Header("Paneles")]
+    [SerializeField] private GameObject menúJuego;
+    [SerializeField] private GameObject menúInicio;
+
     [Header("Paneles")]
     [SerializeField] private GameObject panelInicio;
     [SerializeField] private GameObject panelOpciones;
     [SerializeField] private GameObject panelCréditos;
 
+    [Header("Botones")]
+    [SerializeField] private GameObject btnIniciar;
+    [SerializeField] private GameObject btnReiniciar;
+    [SerializeField] private GameObject btnReanudar;
+
     [Header("Referencias")]
     [SerializeField] private TMP_Text txtVersión;
 
     public static int semilla;
+
+    private ControladorDialogos controladorDiálogos;
 
     private void Start()
     {
@@ -21,16 +31,63 @@ public class ControladorMenu : MonoBehaviour
         var segundos = (int)(DateTime.Now - fecha).TotalSeconds;
         semilla = segundos;
 
+        controladorDiálogos = FindObjectOfType<ControladorDialogos>();
+        controladorDiálogos.gameObject.SetActive(false);
+
+        menúInicio.SetActive(true);
+        menúJuego.SetActive(false);
+
         panelInicio.SetActive(true);
         panelOpciones.SetActive(false);
         panelCréditos.SetActive(false);
+
+        btnIniciar.SetActive(true);
+        btnReiniciar.SetActive(false);
+        btnReanudar.SetActive(false);
 
         txtVersión.text = Application.version;
     }
 
     public void EnClicIniciar()
     {
-        SistemaEscenas.instancia.CambiarEscena(Escenas.Juego);
+        menúInicio.SetActive(false);
+        menúJuego.SetActive(true);
+
+        // PENDIENTE animacion camara
+        controladorDiálogos.gameObject.SetActive(true);
+        controladorDiálogos.ComenzarJuego();
+    }
+
+    public void EnClicReiniciar()
+    {
+        menúInicio.SetActive(false);
+        menúJuego.SetActive(true);
+
+        // PENDIENTE animacion camara
+        controladorDiálogos.gameObject.SetActive(true);
+        controladorDiálogos.ComenzarJuego();
+    }
+
+    public void EnClicReanudar()
+    {
+        menúInicio.SetActive(false);
+        menúJuego.SetActive(true);
+
+        // PENDIENTE animacion camara
+        controladorDiálogos.gameObject.SetActive(true);
+    }
+
+    public void EnClicPausar()
+    {
+        menúInicio.SetActive(true);
+        menúJuego.SetActive(false);
+
+        btnIniciar.SetActive(false);
+        btnReiniciar.SetActive(true);
+        btnReanudar.SetActive(true);
+
+        // PENDIENTE animacion camara
+        controladorDiálogos.gameObject.SetActive(false);
     }
 
     public void EnClicOpciones()
@@ -55,5 +112,16 @@ public class ControladorMenu : MonoBehaviour
     public void EnClicSalir()
     {
         Application.Quit();
+    }
+
+    public void FinalizarJuego()
+    {
+        panelInicio.SetActive(true);
+        panelOpciones.SetActive(false);
+        panelCréditos.SetActive(false);
+
+        btnIniciar.SetActive(true);
+        btnReiniciar.SetActive(false);
+        btnReanudar.SetActive(false);
     }
 }
