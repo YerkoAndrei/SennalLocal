79abcfae5ce65d0d9bf495eb6f2b3d49ede502c3
Yerko Andrei <yerkoandrei@gmail.com>
2023-07-10 21:40:57 -0400

    cable audifonos

---
+++ Assets/Codigo/Visuales/ControladorCable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ControladorCable : MonoBehaviour
+{
+    [SerializeField] private Rigidbody entradaCable;
+    [SerializeField] private Collider[] colisionesCable;
+    [SerializeField] private FixedJoint cable;
+
+    private void Awake()
+    {
+        foreach(var col in colisionesCable)
+        {
+            col.enabled = false;
+        }
+
+        cable.transform.position = entradaCable.position;
+        cable.transform.rotation = entradaCable.rotation;
+
+        cable.connectedBody = entradaCable;
+
+        foreach (var col in colisionesCable)
+        {
+            col.enabled = true;
+        }
+    }
+}
