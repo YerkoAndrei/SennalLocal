7ecef5944e00026415b9e6236fe86f85d2c12616
Yerko Andrei <yerkoandrei@gmail.com>
2023-08-09 16:09:50 -0400

    codigos rutas

--- Assets/Codigo/Visuales/ControladorRadio.cs
+++ Assets/Codigo/Visuales/ControladorRadio.cs
@@ -20,28 +20,28 @@ public class ControladorRadio : MonoBehaviour
         switch(ruta)
         {
             case Rutas.menú:
-                txtRuta.text = "MENÚ";
+                txtRuta.text = "647F";//MENÚ
                 break;
             case Rutas.intro:
-                txtRuta.text = "INTRO";
+                txtRuta.text = "57DB8";//INTRO
                 break;
             case Rutas.operador:
-                txtRuta.text = "OPERADOR";
+                txtRuta.text = "8A4B138B";//OPERADOR
                 break;
             case Rutas.usuario:
-                txtRuta.text = "USUARIO";
+                txtRuta.text = "ECE1B58";//USUARIO
                 break;
             case Rutas.monstruo:
-                txtRuta.text = "MONSTRUO";
+                txtRuta.text = "687CDBE8";//MONSTRUO
                 break;
             case Rutas.caza:
-                txtRuta.text = "CAZA";
+                txtRuta.text = "21G1";//CAZA
                 break;
             case Rutas.sótano:
-                txtRuta.text = "SÓTANO";
+                txtRuta.text = "C9D178";//SÓTANO
                 break;
             case Rutas.autor:
-                txtRuta.text = "AUTOR";
+                txtRuta.text = "1ED8B";//AUTOR
                 break;
         }
     }
@@ -66,3 +66,56 @@ public class ControladorRadio : MonoBehaviour
         CambiarNombreRuta(rutaAnterior);
     }
 }
+
+// Abecedario español
+//01 - a
+//02 - á
+//03 - b
+//04 - c
+//05 - d
+//06 - e
+//07 - é
+//08 - f
+//09 - g
+//10 - h
+//11 - i
+//12 - í
+//13 - j
+//14 - k
+//15 - l
+//16 - m
+//17 - n
+//18 - ñ
+//19 - o
+//20 - ó
+//21 - p
+//22 - q
+//23 - r
+//24 - s
+//25 - t
+//26 - u
+//27 - ú
+//28 - ü
+//29 - v
+//30 - w
+//31 - x
+//32 - y
+//33 - z
+
+// Rutas
+//1 - a
+//2 - c
+//3 - d
+//4 - e
+//5 - i
+//6 - m
+//7 - n
+//8 - o
+//9 - ó
+//A - p
+//B - r
+//C - s
+//D - t
+//E - u
+//F - ú
+//G - z 
